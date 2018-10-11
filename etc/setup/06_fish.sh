cf738fab368a997c946f4895caac87c30ef4fad8
andoshin11 <shinglish11@gmail.com>
2018-10-11 23:50:51 +0900

    Add fish setup script

---
+++ etc/setup/06_fish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+brew install fish
+sudo sh -c "echo '/usr/local/bin/fish' >> /etc/shells"
+sudo chsh -s /usr/local/bin/fish
+
+# Install fisherman
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+
