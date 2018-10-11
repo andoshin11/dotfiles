ae047d224c541dec99b8f3f97dc4a4f8cf99cb2c
andoshin11 <shinglish11@gmail.com>
2018-10-12 00:53:58 +0900

    Add fish config deploy command

--- etc/setup/06_fish.sh
+++ etc/setup/06_fish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 brew install fish
 sudo sh -c "echo '/usr/local/bin/fish' >> /etc/shells"
 sudo chsh -s /usr/local/bin/fish
@@ -6,3 +7,5 @@ sudo chsh -s /usr/local/bin/fish
 # Install fisherman
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
+# deploy config file
+ln -sfvn $PWD/config.fish $HOME/.config/fish/config.fish
