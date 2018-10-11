c24298ecf1162cc117068ffa0375d88b9446259d
andoshin11 <shinglish11@gmail.com>
2018-10-12 00:37:35 +0900

    Update app list to install using mas

--- etc/setup/08_mas.sh
+++ etc/setup/08_mas.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 brew install mas
+
+# Currently not working
+# mas install 497799835 # XCode
+# mas install 1081413713 # GIF Brewery 3 by Gfycat
+# mas install 748212890 # Memory Cleaner: Free Up Memory
+# mas install 504544917 # Clear - Tasks, Reminders & To-Do Lists
+
