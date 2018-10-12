7dba3c9787a1882454112bafe12cc02548d1ee77
andoshin11 <shinglish11@gmail.com>
2018-10-12 18:12:12 +0900

    Fix ssh script

--- scripts/04_ssh.sh
+++ scripts/04_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p $HOME/.ssh
-ssh-keygen -f $HOME/.ssh/id_rsatest
-cp $PWD/etc/ssh/config $HOME/.ssh/config_test
+ssh-keygen -f $HOME/.ssh/id_rsa
+cp $PWD/etc/ssh/config $HOME/.ssh/config
 
