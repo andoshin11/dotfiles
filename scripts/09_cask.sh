9dfdb75116512677c6cd68267ca53240f03a67b3
andoshin11 <shinglish11@gmail.com>
2018-10-12 10:55:28 +0900

    Cask install only non-existed apps

--- scripts/09_cask.sh
+++ scripts/09_cask.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+installed=`brew cask list`
+
 for app in `cat ./etc/cask_list.txt`; do
-    echo "Installing ${app}..."
-    brew cask install $app
+    if [[ -z `echo "${installed}" | grep "^${app}$"` ]]; then
+	echo "$(tput setaf 4)Installing ${app}...$(tput sgr0)"
+        brew cask install $app
+        echo "$(tput setaf 2)Installed ${app}!$(tput sgr0)"
+    else
+        echo "$(tput setaf 3)${app} is already installed. $(tput sgr0)"
+    fi
 done
 
