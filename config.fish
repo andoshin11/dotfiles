0d8556d29011e17bb2d8991f0bd16601c344b5d3
andoshin11 <shinglish11@gmail.com>
2018-10-22 22:15:00 +0900

    Add alias to fish

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@
 alias gs 'git status'
 alias gb 'git branch'
 alias ga 'git add .'
+alias wttr 'curl wttr.in/tokyo'
+alias gt "git log --graph --pretty='format:%C(yellow)%h%Creset %s %Cgreen(%an)%Creset %Cred%d%Creset'"
 
 # exec ls after cd
 function cd
