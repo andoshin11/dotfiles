713699b71eaea092f7a34c4529b3e8e2ae228cb9
andoshin11 <shinglish11@gmail.com>
2019-11-18 16:25:03 +0900

    Update fish settings

--- config.fish
+++ config.fish
@@ -2,8 +2,11 @@
 alias gs 'git status'
 alias gb 'git branch'
 alias ga 'git add .'
+alias gc 'git branch | fzf | xargs git checkout'
 alias wttr 'curl wttr.in/tokyo'
 alias gt "git log --graph --pretty='format:%C(yellow)%h%Creset %s %Cgreen(%an)%Creset %Cred%d%Creset'"
+alias gbm 'git branch --merged'
+alias dm 'diesel migration'
 
 # exec ls after cd
 function cd
@@ -13,5 +16,13 @@ end
 
 # env
 set -x PATH $HOME/.nodebrew/current/bin $PATH
+set -x GOENV_DISABLE_GOPATH 1
 set -x GOPATH $HOME/dev/go
 set -x PATH $GOPATH/bin $PATH
+set -x PATH $HOME/.goenv/bin $PATH
+set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH /usr/local/Cellar/postgresql/11.4/bin $PATH
+set -x PATH $HOME/.pyenv/bin $PATH
+
+status --is-interactive; and source (goenv init -|psub)
+
