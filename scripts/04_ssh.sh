4e6637491d1d49bd8a316e48dcc32f2c0b1725f7
shin-ando <shin.ando@linecorp.com>
2021-03-29 15:31:01 +0900

    Update brew install script

--- scripts/04_ssh.sh
+++ scripts/04_ssh.sh
@@ -4,4 +4,3 @@ mkdir -p $HOME/.ssh
 ssh-keygen -f $HOME/.ssh/id_rsa
 cp $PWD/etc/ssh/config $HOME/.ssh/config
 chmod 600 $HOME/.ssh/config
-
--- scripts/05_brew.sh
+++ scripts/05_brew.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # https://brew.sh/index_ja
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install packages
 installed=`brew list`
@@ -9,10 +9,9 @@ installed=`brew list`
 for app in `cat ./etc/brew_list.txt`; do
   if [[ -z `echo "${installed}" | grep "^${app}$"` ]]; then
     echo "$(tput setaf 4)Installing ${app}...$(tput sgr0)"
-    brew cask install $app
+    brew install $app
     echo "$(tput setaf 2)Install ${app}!$(tput sgr0)"
   else
     echo "$(tput setaf 3)${app} is already installed.$(tput sgr0)"
   fi
 done
-
